6390dad916b7c6880b848405519ee5bc20def69f
hassanmoizdev <hassanmoizdev@gmail.com>
2026-08-04 14:22:51 +0500

    udpate the lab smaple report

--- resources/views/laboratory/lab_samples_report.blade.php
+++ resources/views/laboratory/lab_samples_report.blade.php
@@ -289,7 +289,7 @@
                                         <td>
                                             @if($row['result_status'] === 'completed')
                                                 <a href="{{ route('pathology.print_report', ['lab_patient_id' => $row['lab_patient_id'], 'test_id' => $row['test_id']]) }}"
-                                                    target="_blank" class="hms-lsr-action is-report">
+                                                    onclick="event.preventDefault(); window.promptWithHeaderFooter(withHeader => window.open(window.appendWithHeaderParam(this.href, withHeader), '_blank'));" class="hms-lsr-action is-report">
                                                     Report
                                                     <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/>
